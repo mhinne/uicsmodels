3357cbe7e539d00f7391d665cc0b2cb5df95d603
Max Hinne <mhinne@gmail.com>
2023-03-30 15:06:20 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setip
+
+setup(
+    name='Prob-models',
+    version='0.0.1',
+    url='https://github.com/mhinne/prob-models',
+    author='Max Hinne',
+    author_email='max.hinne@donders.ru.nl',
+    py_modules=['gaussian-processes']
+
+)
