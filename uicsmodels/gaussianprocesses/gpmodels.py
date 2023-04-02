9c5e7da17ef97ad6d766518fe610ef789d6c99da
Max Hinne <mhinne@gmail.com>
2023-04-02 16:37:45 +0200

    Update gpmodels.py

--- uicsmodels/gaussianprocesses/gpmodels.py
+++ uicsmodels/gaussianprocesses/gpmodels.py
@@ -208,7 +208,7 @@ class LatentGPModel(GPModel):
             key, _ = jrnd.split(key)
             initial_position['f'] = sample_latent(key, initial_position)
 
-        return GibbsState(*initial_position, *initial_state.position)       
+        return GibbsState({**initial_position, **initial_state.position})       
     
     #
     def gibbs_fn(self, key, state, log_likelihood=None, temperature=1.0, kwargs=None):
