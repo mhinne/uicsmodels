2a7b1b6cfe1f8851337caeaee7a62578724dff8f
Max Hinne <mhinne@gmail.com>
2023-04-03 14:02:47 +0200

    Update gpmodels.py

--- uicsmodels/gaussianprocesses/gpmodels.py
+++ uicsmodels/gaussianprocesses/gpmodels.py
@@ -426,7 +426,7 @@ class MarginalGPModel(GPModel):
             for k in [kXX, kxX, kxx]:
                 k += jitter * jnp.eye(*k.shape)
 
-            L = jnp.linalg.cholesky(kXX + jitter * jnp.eye(n))
+            L = jnp.linalg.cholesky(kXX + jitter * jnp.eye(self.n))
             alpha = jnp.linalg.solve(L.T, jnp.linalg.solve(L, self.y))
             v = jnp.linalg.solve(L, kxX)
             predictive_mean = jnp.dot(kxX.T, alpha)
